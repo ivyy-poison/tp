498a2befbc86b8e768ef952635f543e69c5be40c
ivyy-poison <tan.yuhao.ivan@gmail.com>
2023-09-30 20:31:53 +0800

    Create new remark command

---
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -0,0 +1,16 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.Model;
+
+public class RemarkCommand extends Command {
+
+    public static final String COMMAND_WORD = "remark";
+
+
+
+    @Override
+    public CommandResult execute(Model model) {
+        return new CommandResult("Remark command executed");
+    }
+
+}
